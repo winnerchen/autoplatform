80153562e84718ac930568f598060713de78d2c1
Chen Yiheng <raily@live.cn>
2017-12-20 16:53:29 +1100

    update auto.sql: delete foreign key and add essential index

--- auto-cms/auto-cms-dao/src/main/resources/auto.sql
+++ auto-cms/auto-cms-dao/src/main/resources/auto.sql
@@ -33,8 +33,25 @@ CREATE TABLE `book` (
 -- ----------------------------
 DROP TABLE IF EXISTS `cms_article`;
 CREATE TABLE `cms_article` (
-  `article_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`article_id`)
+  `article_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章编号',
+  `title` varchar(200) NOT NULL COMMENT '文章标题',
+  `author` varchar(50) DEFAULT NULL COMMENT '文章原作者',
+  `fromurl` varchar(300) DEFAULT NULL COMMENT '转载来源网址',
+  `image` varchar(300) DEFAULT NULL COMMENT '封面图',
+  `keywords` varchar(100) DEFAULT NULL COMMENT '关键字',
+  `description` varchar(500) DEFAULT NULL COMMENT '简介',
+  `type` tinyint(4) NOT NULL DEFAULT '1' COMMENT '类型(1:普通,2:热门...)',
+  `allowcomments` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否允许评论(0:不允许,1:允许)',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态(-1:审核不通过回收站,0:刚发布未审核,1:已审核公开,2:已审核个人)',
+  `content` mediumtext COMMENT '内容',
+  `user_id` int(10) unsigned NOT NULL COMMENT '发布人id',
+  `up` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '顶',
+  `down` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '踩',
+  `readnumber` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '阅读数量',
+  `ctime` bigint(20) unsigned NOT NULL COMMENT '创建时间',
+  `orders` bigint(20) unsigned NOT NULL COMMENT '排序',
+  PRIMARY KEY (`article_id`),
+  KEY `cms_article_orders` (`orders`) USING BTREE COMMENT '排序索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章表';
 
 -- ----------------------------
@@ -46,10 +63,8 @@ CREATE TABLE `cms_article_category` (
   `article_id` int(10) unsigned NOT NULL,
   `category_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`article_category_id`),
-  KEY `cms_article_category_article_id` (`article_id`),
-  KEY `cms_article_category_category_id` (`category_id`),
-  CONSTRAINT `cms_article_category_article_id` FOREIGN KEY (`article_id`) REFERENCES `cms_article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `cms_article_category_category_id` FOREIGN KEY (`category_id`) REFERENCES `cms_category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `cms_article_category_article_id` (`article_id`) USING BTREE COMMENT '文章id索引',
+  KEY `cms_article_category_category_id` (`category_id`) USING BTREE COMMENT '类目id索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章类目表';
 
 -- ----------------------------
@@ -61,10 +76,8 @@ CREATE TABLE `cms_article_tag` (
   `article_id` int(10) unsigned NOT NULL,
   `tag_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`article_tag_id`),
-  KEY `cms_article_tag_article_id` (`article_id`),
-  KEY `cms_article_tag_tag_id` (`tag_id`),
-  CONSTRAINT `cms_article_tag_article_id` FOREIGN KEY (`article_id`) REFERENCES `cms_article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `cms_article_tag_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `cms_tag` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `cms_article_tag_article_id` (`article_id`) USING BTREE COMMENT '文章id索引',
+  KEY `cms_article_tag_tag_id` (`tag_id`) USING BTREE COMMENT '标签id索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章标签表';
 
 -- ----------------------------
@@ -73,8 +86,21 @@ CREATE TABLE `cms_article_tag` (
 DROP TABLE IF EXISTS `cms_category`;
 CREATE TABLE `cms_category` (
   `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '类目编号',
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='类目表';
+  `pid` int(10) unsigned NOT NULL COMMENT '上级编号',
+  `level` tinyint(3) NOT NULL COMMENT '层级',
+  `name` varchar(20) NOT NULL COMMENT '名称',
+  `description` varchar(200) DEFAULT NULL COMMENT '描述',
+  `icon` varchar(50) DEFAULT NULL COMMENT '图标',
+  `type` tinyint(3) NOT NULL DEFAULT '1' COMMENT '类型(1:普通,2:热门...)',
+  `alias` varchar(20) DEFAULT NULL COMMENT '别名',
+  `ctime` bigint(20) unsigned NOT NULL COMMENT '创建时间',
+  `orders` bigint(255) unsigned NOT NULL COMMENT '排序',
+  PRIMARY KEY (`category_id`),
+  KEY `cms_category_orders` (`orders`) USING BTREE COMMENT '排序字段索引',
+  KEY `cms_category_pid` (`pid`) USING BTREE COMMENT '父类目索引',
+  KEY `cms_category_alias` (`alias`) USING BTREE COMMENT '别名索引',
+  KEY `cms_category_level` (`level`) USING BTREE COMMENT '类目层级索引'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='类目表';
 
 -- ----------------------------
 -- Table structure for cms_category_tag
@@ -85,11 +111,30 @@ CREATE TABLE `cms_category_tag` (
   `category_id` int(10) unsigned NOT NULL,
   `tag_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`category_tag_id`),
-  KEY `cms_category_tag_category_id` (`category_id`),
-  KEY `cms_category_tag_tag_id` (`tag_id`),
-  CONSTRAINT `cms_category_tag_category_id` FOREIGN KEY (`category_id`) REFERENCES `cms_category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `cms_category_tag_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `cms_tag` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `cms_category_tag_tag_id` (`tag_id`) USING BTREE COMMENT '标签id索引',
+  KEY `cms_category_tag_category_id` (`category_id`) USING BTREE COMMENT '类目id索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT=' 分类标签表';
+
+-- ----------------------------
+-- Table structure for cms_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_comment`;
+CREATE TABLE `cms_comment` (
+  `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `pid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '回复楼中楼id',
+  `article_id` int(10) unsigned NOT NULL COMMENT '文章id',
+  `user_id` int(10) unsigned NOT NULL COMMENT '用户id',
+  `content` text NOT NULL COMMENT '评论内容',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '状态(-1:审核不通过,0:未审核,1:已审核通过)',
+  `ip` varchar(30) DEFAULT NULL COMMENT '评论人ip地址',
+  `agent` varchar(200) DEFAULT NULL COMMENT '评论人终端信息',
+  `ctime` bigint(20) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`comment_id`),
+  KEY `cms_comment_article_id` (`article_id`),
+  KEY `cms_comment_pid` (`pid`),
+  CONSTRAINT `cms_comment_article_id` FOREIGN KEY (`article_id`) REFERENCES `cms_article` (`article_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `cms_comment_pid` FOREIGN KEY (`pid`) REFERENCES `cms_comment` (`comment_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for cms_tag
@@ -97,7 +142,16 @@ CREATE TABLE `cms_category_tag` (
 DROP TABLE IF EXISTS `cms_tag`;
 CREATE TABLE `cms_tag` (
   `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '标签编号',
-  PRIMARY KEY (`tag_id`)
+  `name` varchar(20) NOT NULL COMMENT '名称',
+  `description` varchar(200) DEFAULT NULL COMMENT '描述',
+  `icon` varchar(50) DEFAULT NULL COMMENT '图标',
+  `type` tinyint(4) NOT NULL DEFAULT '1' COMMENT '类型(1:普通,2:热门...)',
+  `alias` varchar(20) DEFAULT NULL COMMENT '别名',
+  `ctime` bigint(20) unsigned NOT NULL COMMENT '创建时间',
+  `orders` bigint(20) unsigned NOT NULL COMMENT 'orders',
+  PRIMARY KEY (`tag_id`),
+  KEY `cms_tag_orders` (`orders`) USING BTREE COMMENT '排序索引',
+  KEY `cms_tag_alias` (`alias`) USING BTREE COMMENT '别名索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='标签表';
 
 -- ----------------------------
@@ -113,5 +167,4 @@ CREATE TABLE `user` (
   `ctime` bigint(20) DEFAULT NULL,
   `content` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COMMENT='用户表';
-
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8 COMMENT='用户表';
