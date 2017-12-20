3f20e58afe1fcd542ef40cc5ce24008a2d98492a
Chen Yiheng <raily@live.cn>
2017-12-20 20:39:10 +1100

    enable cache in mybatis

--- auto-cms/auto-cms-service/src/main/java/yiheng/chen/cms/service/impl/UserServiceImpl.java
+++ auto-cms/auto-cms-service/src/main/java/yiheng/chen/cms/service/impl/UserServiceImpl.java
@@ -3,7 +3,7 @@ package yiheng.chen.cms.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import yiheng.chen.cms.dao.mapper.UserMapper;
+import yiheng.auto.cms.dao.mapper.UserMapper;
 import yiheng.chen.cms.dao.mapper.UserVOMapper;
 import yiheng.chen.cms.dao.model.UserVO;
 import yiheng.chen.cms.service.UserService;
