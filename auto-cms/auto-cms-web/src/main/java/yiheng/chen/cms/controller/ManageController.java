bbfc0545add1d3c572832f596bb9b925ff2a40ac
Chen Yiheng <raily@live.cn>
2017-12-23 11:37:19 +1100

    add spring security

--- auto-cms/auto-cms-web/src/main/java/yiheng/chen/cms/controller/ManageController.java
+++ auto-cms/auto-cms-web/src/main/java/yiheng/chen/cms/controller/ManageController.java
@@ -19,16 +19,13 @@ public class ManageController {
     private static Logger _log = LoggerFactory.getLogger(ManageController.class);
 
     @RequestMapping(value = {"", "/index"})
-    @ResponseBody
     public Object index() {
         return "/manage/index";
     }
 
     @RequestMapping("/login")
-    @ResponseBody
     public Object login() {
         return "/manage/login";
     }
-
 }
 
