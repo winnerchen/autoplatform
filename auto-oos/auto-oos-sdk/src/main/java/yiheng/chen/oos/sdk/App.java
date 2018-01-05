5bc285e29060fb66b20523355621f8aa5c5c03df
Chen Yiheng <raily@live.cn>
2018-01-05 16:06:19 +1100

    add auto-oos sub project

---
+++ auto-oos/auto-oos-sdk/src/main/java/yiheng/chen/oos/sdk/App.java
@@ -0,0 +1,10 @@
+package yiheng.chen.oos.sdk;
+
+/**
+ * @Author: Yiheng Chen
+ * @Description:
+ * @Date: Created in 16:03 2018/1/5
+ * @Modified by:
+ */
+public class App {
+}
---
+++ auto-oos/auto-oos-web/src/main/java/yiheng/chen/oos/web/controller/AppController.java
@@ -0,0 +1,10 @@
+package yiheng.chen.oos.web.controller;
+
+/**
+ * @Author: Yiheng Chen
+ * @Description:
+ * @Date: Created in 16:04 2018/1/5
+ * @Modified by:
+ */
+public class AppController {
+}
